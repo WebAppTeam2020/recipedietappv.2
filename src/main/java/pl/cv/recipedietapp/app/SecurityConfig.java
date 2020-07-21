63248771896acd7cb487418e3ca26452638f853a
lukasz-pasieczynski <l.pasieczynski@gmail.com>
2020-07-21 23:27:00 +0200

    fixed Security

--- src/main/java/pl/cv/recipedietapp/app/SecurityConfig.java
+++ src/main/java/pl/cv/recipedietapp/app/SecurityConfig.java
@@ -2,13 +2,11 @@ package pl.cv.recipedietapp.app;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import pl.cv.recipedietapp.repository.UserRepository;
 import pl.cv.recipedietapp.service.SpringDataUserDetailsService;
 
 @Configuration
